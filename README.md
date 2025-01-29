5ddd4614405bf6c988cbc330954b55f2b9b6414b
ubuntu <foenix208@gmail.com>
2025-01-29 16:00:33 +0100

    requests and argv

--- README.md
+++ README.md
@@ -4,6 +4,3 @@ google dork
 ```
 allintext:@gmail.com filetype:log
 ```
-
-
-exemple : http://www.bodoi.info/wp-content/uploads/mc4wp-debug.log
--- main.py
+++ main.py
@@ -1,15 +1,29 @@
+import requests
+import sys
 
-file = open("teste.txt")
-lignes = file.readlines()
+
+
+if len (sys.argv ) > 1:
+    url = sys.argv[1]
+else:
+    url = input("Rentrer un url : ")
+r = requests.get(url=url)
+lignes = r.text.split("\n")
 
 cmp = 0
 emails = []
+
 for i in lignes: 
     for y in i.split(" "):
         if "@" in y:
             if not y in emails:
                 emails.append(y)
-                
+
+fichier = open("email.txt","w")
+for email in emails:
+    fichier.write(f"{email}\n")    
+    
+fichier.close()
 print(len(emails))
                 
 
